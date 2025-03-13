22f660c7197849cde873f6f9ca100191091dbbab
Prabhjot Singh <prabhx.jott@gmail.com>
2025-03-13 10:56:42 +0530

    successfully deployed frontend

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,13 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  // base: "/lc-sheet-builder-frontend/",
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  server: {
+    historyApiFallback: true,
   },
 });
